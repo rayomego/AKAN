615f6f66416a80f8d941bc3381b33c68b4f4e8ef
ray omego <ray.omego@student.moringaschool.com>
2022-03-09 23:34:08 +0300

    added function validation to js

--- index.js
+++ index.js
@@ -4,3 +4,41 @@ function formReset()
   document.getElementById("form").reset();
 }
 
+//Checking for the vainputlidity of the user 
+function validation()
+{
+    var yearofbirth = document.myform.yearofbirth.value;
+    var monthofbirth = document.myform.monthofbirth.value;
+    var dateofbirth = document.myform.dateofbirth.value;
+    var chooseGender= document.myform.gender.value;;
+    var current_year=new Date().getFullYear();
+    
+    if((yearofbirth == "") || (yearofbirth.length !=4) || 
+    (yearofbirth < 1900) || ( yearofbirth > current_year)) 
+    {
+            swal("Please provide a valid year of birth between 1900-2020");
+            Event.preventDefault(); 
+            return false;
+    }
+    else if(monthofbirth == "" || monthofbirth.length >2 || monthofbirth > 12) 
+    {
+            swal("Please provide a valid month of birth between 1-12");
+            Event.preventDefault();
+            return false;
+    }
+    else if(dateofbirth == "" || dateofbirth.length >2 || dateofbirth > 31) {
+            swal("Please provide a valid date of birth between 1-31");
+            Event.preventDefault();
+            return false;
+    }
+    else if(chooseGender == ""){
+            swal("You must choose a gender");
+            Event.preventDefault();
+            return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
