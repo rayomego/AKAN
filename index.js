1be7ef0d7c7759f5c9558888f48cd5d7934fbd94
ray omego <ray.omego@student.moringaschool.com>
2022-03-09 23:36:43 +0300

    added determinants to js

--- index.js
+++ index.js
@@ -48,3 +48,75 @@ function day(century, year, month, date)
     return ((((century/4)-2*century-1)+(5*year/4) + (26*(month+1)/10) + date)%7);
 }
 
+//Akan Name Determinant starts here.
+function akanFinder()
+{
+    var days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday"];
+    var maleName = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+    var femaleName = ["Ama", "Akosua", "Adwea", "Abenaa", "Akua", "Yaa", "Afua"];
+    var yearValue = document.getElementById("yearofbirth").value;
+    var century = (yearValue.slice(0,2));
+    var year = (yearValue.slice(2,4));
+    var month = document.getElementById("monthofbirth").value;
+    var date = document.getElementById("dateofbirth").value;
+    var gender = document.getElementById("gender").value;
+    var result=day(century,year,month,date).toFixed(); 
+    let bornDate = "You were born on ";
+    let akanName = ", and your Akan name is "
+    if (gender == "1" && result == 0) {
+        swal(bornDate +days[0]+ akanName +maleName[0]);
+        Event.preventDefault();
+      } 
+    else if (gender == "2" && result == 0) {
+        swal(bornDate +days[0]+ akanName +femaleName[0]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 1) {
+        swal(bornDate +days[1]+ akanName +maleName[1]);
+        Event.preventDefault();
+      } 
+    else if (gender == "2" && result == 1) {
+        swal(bornDate +days[1]+ akanName +femaleName[1]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 2) {
+        swal(bornDate +days[2]+ akanName +maleName[2]);
+        Event.preventDefault();
+      } 
+    else if (gender == "2" && result == 2) {
+        swal(bornDate +days[2]+ akanName +femaleName[2]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 3) {
+        swal(bornDate +days[3]+ akanName +maleName[3]);
+        Event.preventDefault();
+      }
+    else if (gender == "2" && result == 3) {
+        swal(bornDate +days[3]+ akanName +femaleName[3]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 4) {
+        swal(bornDate +days[4]+ akanName +maleName[4]);
+        Event.preventDefault();
+      }
+    else if (gender == "2" && result == 4) {
+        swal(bornDate +days[4]+ akanName +femaleName[4]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 5) {
+        swal(bornDate +days[5]+ akanName +maleName[5]);
+        Event.preventDefault();
+      }
+    else if (gender == "2" && result == 5) {
+        swal(bornDate +days[5]+ akanName +femaleName[5]);
+        Event.preventDefault();
+      }
+    else if (gender == "1" && result == 6) {
+        swal(bornDate +days[6]+ akanName +maleName[6]);
+        Event.preventDefault();
+      }
+    else if (gender == "2" && result == 6) {
+        swal(bornDate +days[6]+ akanName +femaleName[6]);
+        Event.preventDefault();
+      }   
+}
