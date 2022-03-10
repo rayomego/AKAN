427c9f34f9838937c05defa6830053abcad93c32
ray omego <ray.omego@student.moringaschool.com>
2022-03-10 11:38:37 +0300

    changes made to js

--- index.js
+++ index.js
@@ -11,20 +11,4 @@ let show1El = document.getElementById('display');
 
 
 
-  let dateEl = new Date (document.getElementById('date').value);
-  let finalEl = dateEl.getDay();
-  let maleEl = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
-  let femaleEl =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-
-  if (rb1.checked==true){
-   show1El.innerText = "Your name is" + " " +(maleEl[finalEl])
-
-  }
-  else if(rb2.checked==true){
-   show1El.innerText = "Your name is" + " " +(femaleEl[finalEl])
-  }
-
-
-  console.log(finalEl)
-
-};
+  
